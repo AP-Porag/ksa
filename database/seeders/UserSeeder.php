82d7451dea3823923584ff3d39b1d2f200a56a9f
AP-Porag <smauhp@gmail.com>
2023-10-30 00:56:14 +0600

    user completed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -30,19 +30,19 @@ class UserSeeder extends Seeder
                 'phone'             => '012345678910',
                 'date_of_hire'     => '2000-10-21',
             ],
-            [
-                'first_name'        => 'Icert',
-                'last_name'         => 'Last',
-                'username'         => 'icertlast',
-                'email'             => 'icert@app.com',
-                'email_verified_at' => now(),
-                'password'          => Hash::make("12345678"),   // 12345678
-                'user_type'         => User::USER_TYPE_ICERT,
-                'status'            => GlobalConstant::STATUS_ACTIVE,
-                'remember_token'    => Str::random(60),
-                'phone'             => '012345678910',
-                'date_of_hire'     => '2000-10-21',
-            ],
+//            [
+//                'first_name'        => 'Icert',
+//                'last_name'         => 'Last',
+//                'username'         => 'icertlast',
+//                'email'             => 'icert@app.com',
+//                'email_verified_at' => now(),
+//                'password'          => Hash::make("12345678"),   // 12345678
+//                'user_type'         => User::USER_TYPE_ICERT,
+//                'status'            => GlobalConstant::STATUS_ACTIVE,
+//                'remember_token'    => Str::random(60),
+//                'phone'             => '012345678910',
+//                'date_of_hire'     => '2000-10-21',
+//            ],
             [
                 'first_name'        => 'Ksa',
                 'last_name'         => 'Last',
