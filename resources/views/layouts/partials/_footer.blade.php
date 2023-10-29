5d6d1395e3cf5d5d3b155e929b1d05f9bdea702a
AP-Porag <smauhp@gmail.com>
2023-10-30 03:42:23 +0600

    adjustment done

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                © {{ date('Y') }}
+                © {{ date('Y') }} iCert Inc.
             </div>
         </div>
     </div>
